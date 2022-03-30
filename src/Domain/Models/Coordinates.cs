19eaaae0771549da605c829507dd6019e5b18b0e
Joshua Sexton-Jones <33764106+joshsj@users.noreply.github.com>
2022-03-30 23:30:35 +0100

    Implement Coordinates.Distance method

--- src/Domain/Models/Coordinates.cs
+++ src/Domain/Models/Coordinates.cs
@@ -1,3 +1,37 @@
-﻿namespace RendezVous.Domain.Models;
+﻿using RendezVous.Domain.Extensions;
+using static System.Math;
 
-public record Coordinates(double Latitude, double Longitude);
+namespace RendezVous.Domain.Models;
+
+public record Coordinates(double Latitude, double Longitude)
+{
+    /// <remarks>Sourced from https://nssdc.gsfc.nasa.gov/planetary/factsheet/earthfact.html</remarks>
+    public static readonly Distance EarthRadius = Models.Distance.FromKilometers(6371);
+    
+    public Distance Distance(Coordinates other)
+    {
+        var left = new Coordinates(this.Latitude.ToRadians(), this.Longitude.ToRadians());
+        var right = new Coordinates(other.Latitude.ToRadians(), other.Longitude.ToRadians());
+
+        return Haversine(left, right, EarthRadius);
+    }
+
+    /// <summary>Implementation of the Haversine formula. All coordinates must be in radians.</summary>
+    /// <remarks>Adapted from https://www.movable-type.co.uk/scripts/latlong.html</remarks>
+    private static Distance Haversine(Coordinates left, Coordinates right, Distance radius)
+    {
+        double Square(double d) => Pow(d, 2);
+
+        var diffLat = (right.Latitude - left.Latitude);
+        var diffLong = (right.Longitude - left.Longitude);
+
+        // square of half the chord length between the points
+        var a = Square(Sin(diffLat / 2)) + Cos(left.Latitude) * Cos(right.Latitude) * Square(Sin(diffLong / 2));
+
+        // angular distance
+        var c = Atan2(Sqrt(a), Sqrt(1 - a)) * 2;
+        
+        // apply scale using radius
+        return new Distance(radius.Meters * c);
+    }
+}
---
+++ tests/Domain.UnitTests/Models/CoordinatesTests.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using RendezVous.Domain.Models;
+
+namespace RendezVous.Domain.UnitTests.Models;
+
+public class CoordinatesTestes
+{
+    
+    /// <summary>1 Millimeter</summary>
+    private const double Tolerance = 0.001; 
+    
+    [TestCaseSource(nameof(TestData))]
+    public void CanMeasureDistance(Coordinates a, Coordinates b, Distance expected)
+    {
+        var distance = a.Distance(b);
+        
+        // tolerance required
+        Assert.True(distance.Meters - expected.Meters < Tolerance);
+    }
+
+    /// <remarks>
+    /// Coordinates sourced from Google Maps
+    /// Results sourced from https://www.onlineconversion.com/map_greatcircle_distance.htm
+    /// </remarks>
+    public static IEnumerable<TestCaseData> TestData
+    {
+        get
+        {
+            yield return new TestCaseData(
+                new Coordinates(0, 0),
+                new Coordinates(0, 0),
+                new Distance(0));
+            
+            // Sheffield Hallam libraries
+            yield return new TestCaseData(
+                new Coordinates(53.3717553, -1.4928765),
+                new Coordinates(53.380264, -1.4675081),
+                Distance.FromKilometers(1.9305366899397525));
+            
+            // London to Manchester
+            yield return new TestCaseData(
+                new Coordinates(51.5287718, -0.2416802),
+                new Coordinates(53.4723272, -2.2935021),
+                Distance.FromKilometers(256.87057377647767));
+        }
+    }
+}
