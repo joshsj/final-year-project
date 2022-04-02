5d9b238c04610f48555462eac202172a282d7905
Joshua Sexton-Jones <33764106+joshsj@users.noreply.github.com>
2022-04-02 04:28:10 +0100

    Fix timer on Confirm page

--- src/WebUI/ClientApp/src/pages/Confirm.vue
+++ src/WebUI/ClientApp/src/pages/Confirm.vue
@@ -3,51 +3,48 @@ import {onMounted, ref, watch} from "vue";
 import {store} from "@/store";
 import {ClockClient, ConfirmationCodeDto} from "@/api/clients";
 import PageTitle from "@/components/general/PageTitle.vue";
+import {useRouter, onBeforeRouteLeave} from "vue-router";
+import {useCounter} from "@/plugins/counter";
 import {ElMessage} from "element-plus";
-import {useRouter} from "vue-router";
 
 const props = defineProps({
   assignmentId: {type: String, required: true}
 });
 const {back} = useRouter();
 
-const codeHtml = ref<string>("");
-const counter = ref({
-  current: -1,
-  type: "success",
-  down: () => {
-    counter.value.current -= 1;
-
-    if (counter.value.current < 1) {
-      ElMessage.info("Confirmation token expired.");
-      back()
-      return;
-    }
-
-    setTimeout(counter.value.down, 1000);
-  }
-});
+const svgSource = ref("");
+const countdown = useCounter();
 
 const getConfirmationCode = async () => {
-  const {svgSource, timeRemaining} = await store.page.load(
+  const dto = await store.page.load(
       () => new ClockClient().getConfirmationCode(props.assignmentId));
 
-  codeHtml.value = svgSource;
-  counter.value.current = timeRemaining;
-  counter.value.down();
+  svgSource.value = dto.svgSource
+  countdown.start(10);
 };
 
 onMounted(getConfirmationCode);
+
+// ensure counter stops to destroy component
+onBeforeRouteLeave(countdown.stop);
+watch(countdown.current, (x) => {
+    if (x > 0) {
+        return;
+    }
+
+    ElMessage.info("Confirmation code expired.");
+    back();
+});
 </script>
 
 <template>
   <page-title title="Confirm">
-    <template v-if="counter.current > -1">
-      <b>Time Remaining:</b> {{ counter.current }}
+    <template v-if="countdown.counting">
+      <b>Time Remaining</b> ~{{ countdown.current }}s
     </template>
   </page-title>
 
-  <div class="confirmation-code" v-html="codeHtml"/>
+  <div class="confirmation-code" v-html="svgSource"/>
 </template>
 
 <style scoped>
---
+++ src/WebUI/ClientApp/src/plugins/counter.ts
@@ -0,0 +1,31 @@
+﻿import {Ref, ref} from "vue";
+
+const useCounter = (current: Ref<number> = ref(0)) => {
+    const counting = ref(false);
+
+    const count = (to: number, step: number) => {
+        if (!counting.value || current.value === to) {
+            stop();
+            return;
+        }
+        
+        current.value += step;
+        setTimeout(() => count(to, step), 1000);
+    }
+
+    const start = (from: number, to = 0, step = -1) => {
+        counting.value = true;
+        current.value = from;
+
+        count(to, step);
+    }
+    
+    const stop = () => {
+        counting.value = false;
+    }
+
+    return {start, stop, current, counting};
+};
+
+
+export {useCounter};
