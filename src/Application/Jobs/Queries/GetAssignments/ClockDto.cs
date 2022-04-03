8f99074eb06e5865658213f9a510aae192d04d6f
Joshua Sexton-Jones <33764106+joshsj@users.noreply.github.com>
2022-04-03 23:46:24 +0100

    Add missing file

--- src/Application/Jobs/Queries/GetAssignments/ClockDto.cs
+++ src/Application/Jobs/Queries/GetAssignments/ClockDto.cs
@@ -1,11 +1,9 @@
 ﻿using RendezVous.Application.Common.Dtos;
-using RendezVous.Domain.Enums;
 
 namespace RendezVous.Application.Jobs.Queries.GetAssignments;
 
 public class ClockDto : EntityDto
 {
-    public ClockType Type { get; set; }
     public DateTime At { get; set; }
-    public ClockDto? Parent { get; set; }
+    public Guid? ParentId { get; set; }
 }
